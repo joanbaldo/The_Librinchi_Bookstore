1016e7d2e9dd1aec4318618e53bacbd32ab04696
joanbaldo <joanbaldo@hotmail.com>
2023-05-14 17:56:09 +0200

    Login component created & view checked

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Products from './components/Products/Products'
+import Login from './components/Login/Login'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
       <div>
 <Products />
+<Login />
 
       </div>
 
