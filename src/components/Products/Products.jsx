277447ea06de5e8669f9f8195ac6c27009d170c0
joanbaldo <joanbaldo@hotmail.com>
2023-05-14 20:01:37 +0200

    Product components context setup

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+//Importamos useContext y useEffect
+import React, { useContext, useEffect } from 'react'
 
 const Products = () => {
+
+  const {getProducts,products} = useContext(ProductContext)
+//Ahora cuando cargue el componente ejecutara getProducts
+  useEffect(() => {
+      getProducts();
+  }, []);
+
+  //En el return, recorremos el array
   return (
-    <div>Products</div>
+    <div>
+        {products.map((product) => {
+           //Porque si no le devolvemos (return) nada al .map nos devolverá error :"undefined"
+            return (
+                <div key={product.id}>
+                    <p>{product.name}</p>
+                </div>
+            )
+        })}
+    </div>
   )
 }
-
+  {/* <div key={character.id} > Esto nos lo pide React para saber que lo que está iterando es único. */ }
 export default Products
