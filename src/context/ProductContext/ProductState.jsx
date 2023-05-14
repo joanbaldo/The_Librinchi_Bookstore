e47d341d08e5b7dea86e21d478cfd52c802ab11c
joanbaldo <joanbaldo@hotmail.com>
2023-05-14 18:45:24 +0200

    Productstate created

--- src/context/ProductContext/ProductState.jsx
+++ src/context/ProductContext/ProductState.jsx
@@ -4,36 +4,40 @@ import axios from "axios";
 
 //Como Ricky y Morty video pero en products
 const initialState = {
-  products: [],
+    products: [],
 };
 
 export const ProductContext = createContext(initialState);
-
+//productProvider es la función que provee la información 
 export const ProductProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(ProductReducer, initialState);
+    //Inicializamos el reducer (e importamos ProductReducer)
+    //OJO-de la siguiente frase SOLO cambia ProductReducer,e resto es igual para todas.
+    const [state, dispatch] = useReducer(ProductReducer, initialState);
 
-  const getProducts = async () => {
-    try {
-      const res = await axios.get(
-        "http://localhost:3000/products/showProducts"
-      );
-      dispatch({
-        type: "GET_PRODUCTS",
-        payload: res.data.products,
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const getProducts = async () => {
+        try {
+            const res = await axios.get(
+                "http://localhost:3000/products//showAllPandC"
+            );
+            dispatch({
+                type: "GET_PRODUCTS",
+                //Es el que da la orden:->vete a ProductReducer y busca "GET_PRODUCTS"
+                //El nombre que hemos puesto a action.type en ProductReducer
+                payload: res.data.products,
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  return (
-    <ProductContext.Provider
-      value={{
-        products: state.products,
-        getProducts,
-      }}
-    >
-      {children}
-    </ProductContext.Provider>
-  );
+    return (
+        <ProductContext.Provider
+            value={{
+                products: state.products,
+                getProducts,
+            }}
+        >
+            {children}
+        </ProductContext.Provider>
+    );
 };
