51fa9b23c96e9bf3fcf4b4f56a57399399e4822e
joanbaldo <joanbaldo@hotmail.com>
2023-05-15 16:56:22 +0200

    Update product context

--- src/context/ProductContext/ProductState.jsx
+++ src/context/ProductContext/ProductState.jsx
@@ -2,42 +2,58 @@ import React, { createContext, useReducer } from "react";
 import ProductReducer from "./ProductReducer.js";
 import axios from "axios";
 
-//Como Ricky y Morty video pero en products
+const cart = JSON.parse(localStorage.getItem("cart"))
 const initialState = {
-    products: [],
+  products: [],
+  cart: cart ? cart : [],
 };
 
+
 export const ProductContext = createContext(initialState);
-//productProvider es la función que provee la información 
+
 export const ProductProvider = ({ children }) => {
-    //Inicializamos el reducer (e importamos ProductReducer)
-    //OJO-de la siguiente frase SOLO cambia ProductReducer,e resto es igual para todas.
-    const [state, dispatch] = useReducer(ProductReducer, initialState);
+  const [state, dispatch] = useReducer(ProductReducer, initialState);
 
-    const getProducts = async () => {
-        try {
-            const res = await axios.get(
-                "http://localhost:3000/products//showAllPandC"
-            );
-            dispatch({
-                type: "GET_PRODUCTS",
-                //Es el que da la orden:->vete a ProductReducer y busca "GET_PRODUCTS"
-                //El nombre que hemos puesto a action.type en ProductReducer
-                payload: res.data.products,
-            });
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const getProducts = async () => {
+    try {
+      const res = await axios.get(
+        "http://localhost:3000/products/showProducts"
+      );
+      dispatch({
+        type: "GET_PRODUCTS",
+        payload: res.data.products,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return (
-        <ProductContext.Provider
-            value={{
-                products: state.products,
-                getProducts,
-            }}
-        >
-            {children}
-        </ProductContext.Provider>
-    );
+  const addCart = (product) => {
+    dispatch({
+      type: "ADD_CART",
+      payload: product,
+    });
+  };
+
+  const clearCart = () => {
+    dispatch({
+      type: "CLEAR_CART",
+    });
+  };
+
+
+
+  return (
+    <ProductContext.Provider
+      value={{
+        products: state.products,
+        cart: state.cart,
+        getProducts,
+        addCart,
+        clearCart
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
 };
