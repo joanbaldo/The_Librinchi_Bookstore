f14db77c9c5b07b1e5c12f506ce321349ef3c184
joanbaldo <joanbaldo@hotmail.com>
2023-05-15 22:03:04 +0200

    Assets uploaded

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../../context/UserContext/UserState";
 import { UserOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import { Avatar, notification, Badge } from "antd";
-import "./Header.css"
+import "./Header.scss"
 import { ProductContext } from "../../context/ProductContext/ProductState";
 
 const Header = () => {
@@ -21,7 +21,14 @@ const Header = () => {
 
   return (
     <div className="headerContainer">
-        <img id="icon" src="https://img.freepik.com/vector-premium/tienda-online-logo_20448-122.jpg?w=2000"></img>
+      <div>
+        {/* <img src="../src/assets/The_Librinchi_Bookstore.jpg"> </img> */}
+        <img src="../src/assets/The_Librinchi_Bookstore.jpg" alt="" />
+
+      </div>
+      <div> <hr /> </div>
+        {/* <img id="icon" src="https://img.freepik.com/vector-premium/tienda-online-logo_20448-122.jpg?w=100"></img> */}
+        {/* <img id="icon" src="https://img.freepik.com/vector-premium/tienda-online-logo_20448-122.jpg?w=100"></img> */}
       {token? (
         <div>
           <Link className="links" to="/products/showProducts">Products | </Link>
@@ -37,13 +44,14 @@ const Header = () => {
           <span className="links" onClick={() => logout()}> Logout</span>
         </div>
         ) : (
-        <div className="navBar">
-          <Link className="links" to="/login">Login |</Link>
-          <Link className="links" to="/users/createUser">Register |</Link>
-          <Link className="links" to="/products/showProducts">Products |</Link>
-          <Link className="links" to="/">Home </Link>
+          <div className="navBar">
+          <Link className="links" to="/">Home |</Link>
+          <Link className="links" to="/products/showProducts"> Products |</Link>
+          <Link className="links" to="/users/createUser"> Register |</Link>
+          <Link className="links" to="/login"> Login </Link>
         </div>
       )}
+          <div> <hr /> </div>
     </div>
   );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
+import "./Home.scss"
 
 const Home = () => {
   return (
-    <div>
-      <span>BOOK CATEGORIES</span>
-      <div>Romantic</div>
-      <div>Science-fiction</div>
-      <div>Drama</div>
-      <div>Fantasy</div>
-      <div>Biography</div>
+    <div className='dHome'>
+      <div className='cattit'>BOOK CATEGORIES</div>
+
+      <div className='dcat'>
+      <div className='dcatlabel'>Romantic</div>
+      <img src="../src/assets/C_Romantic.jpg" alt="Romantic" />
+      </div>
+
+      <div className='dcat'>
+      <div className='dcatlabel'>SciFi</div>
+      <img src="../src/assets/C_Scify.jpg" alt="Scify" />
+      </div>
+
+      <div className='dcat'>
+      <div className='dcatlabel'>Drama</div>
+      <img src="../src/assets/C_Drama.jpg" alt="Drama" />
+      </div>
+
+      <div className='dcat'>
+      <div className='dcatlabel'>Fantasy</div>
+      <img src="../src/assets/C_Fantasy.jpg" alt="Fantasy" />
+      </div>
+
+      <div className='dcat'>
+      <div className='dcatlabel'>Biography</div>
+      <img src="../src/assets/C_Biography.jpg" alt="Romantic" />
+      </div>
 
     </div>
   )
