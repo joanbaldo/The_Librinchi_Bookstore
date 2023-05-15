c0a0475e15c726b5bc558eea20b979ef2a192362
joanbaldo <joanbaldo@hotmail.com>
2023-05-15 16:44:30 +0200

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,42 @@
-import { useState } from 'react'
-import './App.css'
-import Products from './components/Products/Products'
-//Ahora nos importamos el ProductProvider 
-import { ProductProvider } from './context/ProductContext/ProductState'
-
-import Login from './components/Login/Login'
-import Register from './components/Register/Register'
-import Header from './components/Header/Header'
-import Home from './components/Home/Home'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Products from "./components/Products/Products";
+import Register from "./components/Register/Register";
+import { ProductProvider } from "./context/ProductContext/ProductState";
+import { UserProvider } from "./context/UserContext/UserState";
+import Login from "./components/Login/Login";
+import Profile from "./components/Profile/Profile";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/header";
+import Cart from "./components/Cart/Cart";
+import { OrdersProvider } from "./context/OrdersContext/OrdersState";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <ProductProvider>
-          <Header />
-          <Home />
-          <Products />
-          <Register />
-          <Login />
-        </ProductProvider>
-      </div>
-
-    </>
-  )
+    <div>
+      <>
+        <BrowserRouter>
+          <UserProvider>
+            <ProductProvider>
+              <OrdersProvider>
+                <Header />
+                <Routes>
+                  <Route path="/users/createUser" element={<Register />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/products/showProducts" element={<Products />} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/" element={<Home />} />
+                  <Route path="/cart" element={<Cart />} />
+                </Routes>
+                <Footer />
+              </OrdersProvider>
+            </ProductProvider>
+          </UserProvider>
+        </BrowserRouter>
+      </>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,5 +7,51 @@ import { ProductContext } from "../../context/ProductContext/ProductState";
 //Importamos la hormiga y a su familia
 import { UserOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import { Avatar, notification, Badge } from "antd";
-//Estilos
+
 import "./Header.css"
+
+const Header = () => {
+  //Guardamos token,logout y logout msg de UserContext
+  const { token, logout, logoutMessage } = useContext(UserContext)
+  const { cart } = useContext(ProductContext)
+  const navigate = useNavigate();
+  useEffect(() => {
+    //Si hay msg de logout redirige a home y lanza notificación
+    if (logoutMessage) {
+      navigate("/");
+      notification.success({
+        message: logoutMessage,
+      });
+    }
+  }, [logoutMessage]);
+
+  return (
+  <div className="headerContainer">
+  <img id="icon" src="https://img.freepik.com/vector-premium/tienda-online-logo_20448-122.jpg?w=2000"></img>
+{token? (
+          <div>
+          <Link className="links" to="/products/showProducts">Products | </Link>
+          <Link className="links" to="/profile">
+            <Avatar icon={<UserOutlined />} />
+            Profile |
+          </Link>
+          <Link className="links" to="/cart">
+            <Badge className="links" count={cart.length}>
+            <ShoppingCartOutlined /> Cart |
+            </Badge>
+          </Link>
+          <span className="links" onClick={() => logout()}> Logout</span>
+        </div>
+        ) : (
+          <div className="navBar">
+            <Link className="links" to="/login">Login |</Link>
+            <Link className="links" to="/users/createUser">Register |</Link>
+            <Link className="links" to="/products/showProducts">Products |</Link>
+            <Link className="links" to="/">Home </Link>
+          </div>
+  )
+}
+      </div >
+    );
+  };
+export default Header;
