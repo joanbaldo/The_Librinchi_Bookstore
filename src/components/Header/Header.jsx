3bb78b9338399fc98b99e8f0b72da4e8c39164ce
joanbaldo <joanbaldo@hotmail.com>
2023-05-15 07:53:08 +0200

    Header setup 1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
-
-export default Header
+//Importamos react,los hooks de carga y contexto
+import React, { useContext, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+//Se importan los contextos
+import { UserContext } from "../../context/UserContext/UserState";
+import { ProductContext } from "../../context/ProductContext/ProductState";
+//Importamos la hormiga y a su familia
+import { UserOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import { Avatar, notification, Badge } from "antd";
+//Estilos
+import "./Header.css"
